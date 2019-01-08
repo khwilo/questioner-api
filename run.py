b72896b734eb70482e6df8087ac256db1ea74a0a
khwilo <khwilowatai@gmail.com>
2019-01-09 00:28:56 +0300

    [Chore #163065448] Define how the application is to be run

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+from app import create_app
 
-@app.route("/")
-def welcome():
-    return 'Welcome to Questioner'
+CONFIG_NAME = os.getenv('APP_SETTINGS')
+
+app = create_app(CONFIG_NAME)
 
 if __name__ == "__main__":
     app.run()
