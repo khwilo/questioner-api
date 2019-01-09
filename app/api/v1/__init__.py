f24b5c8093479ce0e989c87b6fc14673dd8826ce
khwilo <khwilowatai@gmail.com>
2019-01-09 12:59:52 +0300

    [Feature #163036861] Modularize the application using Blueprints

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,13 @@
+'''
+Modularize the app using BluePrints and the API resource
+'''
+from flask import Blueprint
+from flask_restful import Api
+
+from app.api.v1.views.user_view import UserRegistration
+
+AUTH_BLUEPRINT = Blueprint("auth", __name__, url_prefix='/auth')
+
+AUTH_API = Api(AUTH_BLUEPRINT)
+
+AUTH_API.add_resource(UserRegistration, '/register')
