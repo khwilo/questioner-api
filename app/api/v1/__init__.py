23336683e11e1cf650e3f483c293910e71523a6e
khwilo <khwilowatai@gmail.com>
2019-01-11 09:32:40 +0300

    [Finishes #163036725] Add the route to respond to a meetup RSVP

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,8 +5,9 @@ from flask import Blueprint
 from flask_restful import Api
 
 from app.api.v1.views.meetup_view import MeetupList, Meetup, UpcomingMeetup
-from app.api.v1.views.user_view import UserRegistration, UserLogin
 from app.api.v1.views.question_view import Question, Upvote, Downvote
+from app.api.v1.views.rsvp_view import Rsvp
+from app.api.v1.views.user_view import UserRegistration, UserLogin
 
 AUTH_BLUEPRINT = Blueprint("auth", __name__, url_prefix='/auth')
 API_BLUEPRINT = Blueprint("api", __name__, url_prefix='/api/v1')
@@ -23,3 +24,4 @@ API.add_resource(UpcomingMeetup, '/meetups/upcoming/')
 API.add_resource(Question, '/meetups/<meetup_id>/questions')
 API.add_resource(Upvote, '/meetups/<meetup_id>/questions/<question_id>/upvote')
 API.add_resource(Downvote, '/meetups/<meetup_id>/questions/<question_id>/downvote')
+API.add_resource(Rsvp, '/meetups/<meetup_id>/rsvps')
