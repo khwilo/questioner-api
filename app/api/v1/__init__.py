c00d7ab7bcc4808358cce1e497305a44cb8ff0cd
khwilo <khwilowatai@gmail.com>
2019-01-11 08:17:30 +0300

    [Finishes #163036326] Add the upvote and downvote routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 
 from app.api.v1.views.meetup_view import MeetupList, Meetup, UpcomingMeetup
 from app.api.v1.views.user_view import UserRegistration, UserLogin
-from app.api.v1.views.question_view import Question
+from app.api.v1.views.question_view import Question, Upvote, Downvote
 
 AUTH_BLUEPRINT = Blueprint("auth", __name__, url_prefix='/auth')
 API_BLUEPRINT = Blueprint("api", __name__, url_prefix='/api/v1')
@@ -21,3 +21,5 @@ API.add_resource(MeetupList, '/meetups')
 API.add_resource(Meetup, '/meetups/<meetup_id>')
 API.add_resource(UpcomingMeetup, '/meetups/upcoming/')
 API.add_resource(Question, '/meetups/<meetup_id>/questions')
+API.add_resource(Upvote, '/meetups/<meetup_id>/questions/<question_id>/upvote')
+API.add_resource(Downvote, '/meetups/<meetup_id>/questions/<question_id>/downvote')
