c1ac73f77c319b103f0c7e20aad2dceeeb77fbb5
khwilo <khwilowatai@gmail.com>
2019-01-15 23:55:12 +0300

    [Chore #163225021] Implement the validations on the views

--- app/api/v1/views/meetup_view.py
+++ app/api/v1/views/meetup_view.py
@@ -1,4 +1,5 @@
 '''This module represents the meetup view'''
+from flask import abort
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import reqparse, Resource
 
@@ -23,9 +24,7 @@ class MeetupList(Resource):
         user = UserModel.get_user_by_username(current_user)
 
         if not user:
-            return {
-                'message': "This action required loggin in!"
-            }, 401
+            abort(401, 'This action required loggin in!')
 
         if user['is_admin']:
             meetup = MeetupModel(
@@ -40,9 +39,8 @@ class MeetupList(Resource):
                 'status': 201,
                 'data': [Utility.serialize(meetup)]
             }, 201
-        return {
-            'message': "Only administrators can create a meetup"
-        }, 403
+        abort(403, "Only administrators can create a meetup")
+        return None
 
 class Meetup(Resource):
     '''Request on a meetup item'''
@@ -52,16 +50,13 @@ class Meetup(Resource):
         if meetup_id.isdigit():
             meetup = MeetupModel.get_meetup_by_id(int(meetup_id))
             if meetup == {}:
-                return {
-                    'message': "Meetup with id '{}' doesn't exist!".format(meetup_id)
-                }, 404
+                abort(404, "Meetup with id '{}' doesn't exist!".format(meetup_id))
             return {
                 'status': 200,
                 'data': [meetup]
             }, 200
-        return {
-            'message': 'Meetup ID must be an Integer'
-        }, 400
+        abort(400, 'Meetup ID must be an Integer')
+        return None
 
 class UpcomingMeetup(Resource):
     '''Request on an upcoming meetup item'''
@@ -70,9 +65,7 @@ class UpcomingMeetup(Resource):
         '''Fetch all upcoming meetups'''
         meetups = MeetupModel.get_all_meetups()
         if meetups == []:
-            return {
-                'message': 'No meetup is available'
-            }, 404
+            abort(404, 'No meetup is available')
         return {
             'status': 200,
             'data': sorted(meetups, key=lambda item: item['happening_on'], reverse=True)
