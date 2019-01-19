c0889053fc59f1dbe9c55e52b57c2ecdde219644
khwilo <khwilowatai@gmail.com>
2019-01-19 22:20:35 +0300

    [Feature #163302469] Initialize and destroy the db on setUp and tearDown

--- app/tests/v2/test_base_v2.py
+++ app/tests/v2/test_base_v2.py
@@ -1,9 +1,8 @@
 '''This module represents the base test class for v2 of the API'''
-import os
 import unittest
 
 from app import create_app
-from app.api.v2.database.database_config import init_test_db, destroy
+from app.api.v2.models.database import DatabaseSetup
 
 class BaseTestDbTestCase(unittest.TestCase):
     '''Base class for other test classes for API v2'''
@@ -15,20 +14,19 @@ class BaseTestDbTestCase(unittest.TestCase):
             firstname="test_first",
             lastname="test_last",
             othername="test_other",
-            email="t@example.com",
+            email="test@example.com",
             phone_number="0700000000",
-            username="t_user",
+            username="test_user",
             is_admin=False,
             password="12345"
         )
 
         with self.app.app_context():
-            self.database = init_test_db()
+            DatabaseSetup.initialize_db()
 
     def tearDown(self):
         with self.app.app_context():
-            destroy()
-            self.database.close()
+            DatabaseSetup.destroy()
 
     def get_accept_content_type_headers(self):
         '''Return the content type headers for the body'''
