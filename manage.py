4719978ee09b0c1fce26e9ea415f2da47da43253
khwilo <khwilowatai@gmail.com>
2019-01-21 00:29:56 +0300

    [Feature #163302469] Write explicit queries for migrations

--- manage.py
+++ manage.py
@@ -2,22 +2,35 @@
 import os
 import psycopg2
 
-from db import create_tables, create_admin
+from app.api.v1.models.user_model import UserModel
+from db import create_tables
 
 def migrate_main():
     '''Perform database migrations for the main db'''
     database_url = os.getenv('DATABASE_URL')
     connection = psycopg2.connect(database_url)
     create_tables(connection)
-    create_admin(connection)
+    query = """INSERT INTO users(
+    firstname, lastname, othername, email, phone_number, username, is_admin, password) \
+    VALUES('Khwilo', 'Kabaka', 'Watai', 'watai@questioner.com', '0700000000', 'watai', True, '{}')"""\
+    .format(UserModel.generate_password_hash('questioner_1234'))
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
 
 def migrate_test():
     '''Perform database migrations for the test db'''
     database_url = os.getenv('DATABASE_TEST_URL')
     connection = psycopg2.connect(database_url)
     create_tables(connection)
-    create_admin(connection)
+    query = """INSERT INTO users(
+    firstname, lastname, othername, email, phone_number, username, is_admin, password) \
+    VALUES('Khwilo', 'Kabaka', 'Watai', 'watai@questioner.com', '0700000000', 'watai', True, '{}')"""\
+    .format(UserModel.generate_password_hash('questioner_1234'))
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
 
 if __name__ == '__main__':
-    # migrate_main()
+    migrate_main()
     migrate_test()
