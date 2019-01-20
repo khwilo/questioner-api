0f27b5e3bf55586188d9285c6c028d68364ff906
khwilo <khwilowatai@gmail.com>
2019-01-20 19:12:36 +0300

    [Feature #163302469] Establish the database connection

--- app/api/v2/models/base_model.py
+++ app/api/v2/models/base_model.py
@@ -1,5 +1,4 @@
 '''Module representing the base model'''
-from flask import g
 from psycopg2.extras import RealDictCursor
 
 from db import establish_connection
@@ -7,15 +6,8 @@ from db import establish_connection
 class BaseModel:
     '''Definitions for the base model'''
     def __init__(self):
-        self.connection = BaseModel.check_connection()
+        self.connection = establish_connection()
         self.cursor = self.connection.cursor(cursor_factory=RealDictCursor)
-
-    @classmethod
-    def check_connection(cls):
-        '''Check if the database connection exists'''
-        if 'connection' not in g:
-            connection = establish_connection()
-        return connection
 
     def find_item_if_exists(self, tablename, column, value):
         """Find an item if it exists"""
