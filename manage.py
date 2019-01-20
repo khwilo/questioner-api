f73e53449c5b8ee4682ee9d6566a2bdcd9d41901
khwilo <khwilowatai@gmail.com>
2019-01-20 23:57:42 +0300

    [Feature #163302469] Define methods to migrate the database

---
+++ manage.py
@@ -0,0 +1,23 @@
+'''Module to seed the database with an admin user'''
+import os
+import psycopg2
+
+from db import create_tables, create_admin
+
+def migrate_main():
+    '''Perform database migrations for the main db'''
+    database_url = os.getenv('DATABASE_URL')
+    connection = psycopg2.connect(database_url)
+    create_tables(connection)
+    create_admin(connection)
+
+def migrate_test():
+    '''Perform database migrations for the test db'''
+    database_url = os.getenv('DATABASE_TEST_URL')
+    connection = psycopg2.connect(database_url)
+    create_tables(connection)
+    create_admin(connection)
+
+if __name__ == '__main__':
+    migrate_main()
+    migrate_test()
