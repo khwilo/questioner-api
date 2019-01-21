b5aa91af3abf86d04070f13bdadfb1165796b164
khwilo <khwilowatai@gmail.com>
2019-01-21 23:45:47 +0300

    [Feature #163355954] Add the route to log in a user as a resource

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -5,9 +5,11 @@ from flask import Blueprint
 from flask_restful import Api
 
 from app.api.v2.views.user_view import UserRegistration as registration_v2
+from app.api.v2.views.user_view import UserLogin as login_v2
 
 API_V2_BLUEPRINT = Blueprint("api_v2", __name__, url_prefix="/api/v2")
 
 API_V2 = Api(API_V2_BLUEPRINT)
 
 API_V2.add_resource(registration_v2, '/register')
+API_V2.add_resource(login_v2, '/auth/login')
