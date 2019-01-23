4340d09a9c7bfb977ab2cb5acb69fc4133323652
khwilo <khwilowatai@gmail.com>
2019-01-23 22:50:55 +0300

    [Finishes #163371781] Test that the API cannot fetch a meetup which doesn't exist

--- app/tests/v2/test_meetup.py
+++ app/tests/v2/test_meetup.py
@@ -1,7 +1,7 @@
 '''This module represents tests for the meetup entity'''
 import json
 from app.tests.v2.sample_data import ADMIN_LOGIN, MEETUP, USER_REGISTRATION, \
-USER_LOGIN, NEW_MEETUP
+USER_LOGIN
 from app.tests.v2.test_base import BaseTestCase
 
 class MeetupTestCase(BaseTestCase):
@@ -37,10 +37,6 @@ class MeetupTestCase(BaseTestCase):
             "Only administrators can create a meetup"
         )
 
-    def test_fetch_one_meetup(self):
-        '''Test the API can fetch one meetup'''
-        pass
-
     def test_fetch_incorrect_meetup_id(self):
         '''Test the API cannot fetch a meetup with an incorrect ID'''
         access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
@@ -54,7 +50,14 @@ class MeetupTestCase(BaseTestCase):
 
     def test_empty_meetup_item(self):
         '''Test the API cannot read data from an empty meetup item'''
-        pass
+        access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
+        res = self.client().get(
+            '/api/v2/meetups/2',
+            headers=self.get_authentication_headers(access_token)
+        )
+        self.assertEqual(res.status_code, 404)
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(response_msg["message"]["error"], "Meetup with id '2' doesn't exist!")
 
     def test_fetch_all_meetups(self):
         '''Test the API can fetch all meetups'''
