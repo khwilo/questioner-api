87ddb490109de8f27b68f40d26982f7687e3bc75
khwilo <khwilowatai@gmail.com>
2019-01-24 13:53:39 +0300

    [Starts #163372308] Add test for asking a question

--- app/tests/v2/test_question.py
+++ app/tests/v2/test_question.py
@@ -1,20 +1,28 @@
-'''This module represents tests for the question entity'''
+"""This module represents tests for the question entity"""
+import json
+
+from app.tests.v2.sample_data import ADMIN_LOGIN, USER_REGISTRATION, \
+USER_LOGIN, MEETUP, QUESTION
 from app.tests.v2.test_base import BaseTestCase
 
 class QuestionTestCase(BaseTestCase):
-    '''Test definitions for a question'''
-    def test_create_question(self):
-        '''Test the API can create a question'''
-        pass
-
+    """Test definitions for a question"""
     def test_ask_non_existent_meetup(self):
-        '''Test the API cannot post a question to a non-existent meetup'''
-        pass
+        """Test the API cannot post a question to a non-existent meetup"""
+        access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
+        res = self.client().post(
+            '/api/v2/meetups/3/questions',
+            headers=self.get_authentication_headers(access_token),
+            data=json.dumps(QUESTION)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(response_msg["message"]["error"], "Meetup with id '3' doesn't exist!")
 
     def test_upvote_question(self):
-        '''Test the API can upvote a question'''
+        """Test the API can upvote a question"""
         pass
 
     def test_downvote_question(self):
-        '''Test the API can downvote a question'''
+        """Test the API can downvote a question"""
         pass
