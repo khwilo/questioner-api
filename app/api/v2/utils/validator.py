c53994de1ae927a8c0437454ef48729f069b8588
khwilo <khwilowatai@gmail.com>
2019-01-25 08:43:53 +0300

    [Chore #163468739] Validate the password length

--- app/api/v2/utils/validator.py
+++ app/api/v2/utils/validator.py
@@ -44,6 +44,13 @@ class ValidationHandler:
                 "error": "password cannot be empty",
                 "status": 400
             })
+        if len(password) < 8:
+            abort(
+                400, {
+                    "error": "Password is less than eight characters",
+                    "status": 400
+                }
+            )
 
     @staticmethod
     def validate_email_address(email):
