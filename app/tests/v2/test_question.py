062bd34f8c604687e7a5385846a6e943c69d95b6
khwilo <khwilowatai@gmail.com>
2019-01-26 20:37:26 +0300

    [Bug #163498385] Test a user can ask or vote on a question

--- app/tests/v2/test_question.py
+++ app/tests/v2/test_question.py
@@ -1,12 +1,34 @@
 """This module represents tests for the question entity"""
 import json
 
-from app.tests.v2.sample_data import USER_REGISTRATION, USER_LOGIN, QUESTION
+from app.tests.v2.sample_data import USER_REGISTRATION, USER_LOGIN, QUESTION, \
+ADMIN_LOGIN, MEETUP, NEW_USER_REGISTRATION, NEW_USER_LOGIN
 from app.tests.v2.test_base import BaseTestCase
 
 class QuestionTestCase(BaseTestCase):
     """Test definitions for a question"""
-    def test_ask_non_existent_meetup(self):
+    def test_ask_question_meetup(self):
+        """Test the API can post a question to a meetup"""
+        res = self.client().post(
+            '/api/v2/auth/login',
+            headers=self.get_accept_content_type_headers(),
+            data=json.dumps(ADMIN_LOGIN)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
+        self.create_meetup(access_token, MEETUP)
+        access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
+        res = self.client().post(
+            '/api/v2/meetups/1/questions',
+            headers=self.get_authentication_headers(access_token),
+            data=json.dumps(QUESTION)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(response_msg["message"], "Question created successfully!")
+
+
+    def test_ask_question_non_existent_meetup(self):
         """Test the API cannot post a question to a non-existent meetup"""
         access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
         res = self.client().post(
@@ -18,7 +40,7 @@ class QuestionTestCase(BaseTestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(response_msg["message"]["error"], "Meetup with id '3' doesn't exist!")
 
-    def test_incorrect_meetup_id(self):
+    def test_ask_question_incorrect_meetup_id(self):
         """Test an incorrect meetup ID is validated"""
         access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
         res = self.client().post(
@@ -29,6 +51,27 @@ class QuestionTestCase(BaseTestCase):
         response_msg = json.loads(res.data.decode("UTF-8"))
         self.assertEqual(res.status_code, 400)
         self.assertEqual(response_msg["message"]["error"], "Meetup ID must be an integer value")
+
+    def test_vote_question(self):
+        """Test the API can place a vote on a question"""
+        res = self.client().post(
+            '/api/v2/auth/login',
+            headers=self.get_accept_content_type_headers(),
+            data=json.dumps(ADMIN_LOGIN)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
+        self.create_meetup(access_token, MEETUP)
+        access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
+        self.create_question(access_token, QUESTION)
+        access_token = self.get_access_token(NEW_USER_REGISTRATION, NEW_USER_LOGIN)
+        res = self.client().patch(
+            '/api/v2/questions/1/upvote',
+            headers=self.get_authentication_headers(access_token)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(response_msg["data"][0]["votes"], 1)
 
     def test_vote_with_incorrect_question_id(self):
         """Test validate question ID on voting"""
