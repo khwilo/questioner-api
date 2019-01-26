b5efe4333366867605dfb9580d3508e34ea8541e
khwilo <khwilowatai@gmail.com>
2019-01-26 20:13:17 +0300

    [Bug #163498385] Add tests for fetching a single meetup

--- app/tests/v2/test_meetup.py
+++ app/tests/v2/test_meetup.py
@@ -37,6 +37,25 @@ class MeetupTestCase(BaseTestCase):
             "Only administrators can create a meetup"
         )
 
+    def test_fetch_one_meetup(self):
+        """Test the API can fetch one meetup item"""
+        res = self.client().post(
+            '/api/v2/auth/login',
+            headers=self.get_accept_content_type_headers(),
+            data=json.dumps(ADMIN_LOGIN)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
+        self.create_meetup(access_token, MEETUP)
+        res = self.client().get(
+            '/api/v2/meetups/1',
+            headers=self.get_authentication_headers(access_token)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(response_msg["data"]["location"], "Test Location")
+
+
     def test_fetch_incorrect_meetup_id(self):
         """Test the API cannot fetch a meetup with an incorrect ID"""
         access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
@@ -66,8 +85,8 @@ class MeetupTestCase(BaseTestCase):
             headers=self.get_accept_content_type_headers(),
             data=json.dumps(ADMIN_LOGIN)
         )
-        respone_msg = json.loads(res.data.decode("UTF-8"))
-        access_token = respone_msg["data"][0]["token"]
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
         self.create_meetup(access_token, MEETUP) # Earlier meetup
         self.create_meetup(access_token, NEW_MEETUP) # Latest meetup
         res = self.client().get(
@@ -77,7 +96,7 @@ class MeetupTestCase(BaseTestCase):
         response_msg = json.loads(res.data.decode("UTF-8"))
         self.assertEqual(res.status_code, 200)
         self.assertTrue(response_msg["data"])
-        self.assertTrue(respone_msg["data"][0], NEW_MEETUP)
+        self.assertTrue(response_msg["data"][0], NEW_MEETUP)
 
     def test_fetch_empty_meetup_list(self):
         """Test the API cannot fetch data from an empty meetup list data store"""
@@ -90,8 +109,29 @@ class MeetupTestCase(BaseTestCase):
         response_msg = json.loads(res.data.decode("UTF-8"))
         self.assertEqual(response_msg["message"]["error"], 'No meetup is available')
 
-    def test_admin_delete_meetup(self):
-        """Test the API can delete a meetup record"""
+    def test_admin_can_delete_a_meetup(self):
+        """Test that an admin user can delete a meetup"""
+        res = self.client().post(
+            '/api/v2/auth/login',
+            headers=self.get_accept_content_type_headers(),
+            data=json.dumps(ADMIN_LOGIN)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
+        self.create_meetup(access_token, NEW_MEETUP)
+        res = self.client().delete(
+            '/api/v2/meetups/1',
+            headers=self.get_authentication_headers(access_token)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(
+            response_msg["message"],
+            "Meetup with ID '1' has been successfully deleted"
+        )
+
+    def test_admin_delete_non_existent_meetup(self):
+        """Test the API can verify if the meetup to delete is available"""
         res = self.client().post(
             '/api/v2/auth/login',
             headers=self.get_accept_content_type_headers(),
@@ -100,11 +140,29 @@ class MeetupTestCase(BaseTestCase):
         response_msg = json.loads(res.data.decode("UTF-8"))
         access_token = response_msg["data"][0]["token"]
         res = self.client().delete(
-            '/api/v2/meetups/1',
+            '/api/v2/meetups/4',
             headers=self.get_authentication_headers(access_token)
         )
         response_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual(response_msg["message"]['error'], "Meetup with ID '1' has been removed!")
+        self.assertEqual(response_msg["message"]['error'], "Meetup with ID '4' doesn't exist!")
+        self.assertEqual(res.status_code, 404)
+
+    def test_admin_delete_meetup_with_wrong_id(self):
+        """Test the API can validate a wrong meetup ID"""
+        res = self.client().post(
+            '/api/v2/auth/login',
+            headers=self.get_accept_content_type_headers(),
+            data=json.dumps(ADMIN_LOGIN)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
+        res = self.client().delete(
+            '/api/v2/meetups/b',
+            headers=self.get_authentication_headers(access_token)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(response_msg["message"]['error'], "Meetup ID must be an Integer")
+        self.assertEqual(res.status_code, 400)
 
     def test_regular_user_cannot_delete_meetup(self):
         """Test the API cannot delete a meetup on wrong authorization"""
