68d5694b373f420a63bbae529dd67b55466268c3
khwilo <khwilowatai@gmail.com>
2019-01-27 18:54:13 +0300

    [Feature #163501774] Test the API can validate if the vote is an upvote/downvote

--- app/tests/v2/test_question.py
+++ app/tests/v2/test_question.py
@@ -94,6 +94,30 @@ class QuestionTestCase(BaseTestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(response_msg["data"][0]["votes"], -1)
 
+    def test_wrong_vote_parameter(self):
+        """Test the API can validate if a vote is either an upvote/downvote"""
+        res = self.client().post(
+            '/api/v2/auth/login',
+            headers=self.get_accept_content_type_headers(),
+            data=json.dumps(ADMIN_LOGIN)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
+        self.create_meetup(access_token, MEETUP)
+        access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
+        self.create_question(access_token, QUESTION)
+        access_token = self.get_access_token(NEW_USER_REGISTRATION, NEW_USER_LOGIN)
+        res = self.client().patch(
+            '/api/v2/questions/1/vote',
+            headers=self.get_authentication_headers(access_token)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(
+            response_msg["message"]["error"],
+            "Vote path parameter can either be upvote / downvote"
+        )
+
     def test_vote_with_incorrect_question_id(self):
         """Test validate question ID on voting"""
         access_token = self.get_access_token(USER_REGISTRATION, USER_LOGIN)
