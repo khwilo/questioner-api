5faa79766d0704f36c1bd599904ecfdbf84ed515
khwilo <khwilowatai@gmail.com>
2019-01-27 07:41:02 +0300

    [Feature #163501774] Add the vote type to the path

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 
 from app.api.v2.views.comment_view import Comment
 from app.api.v2.views.meetup_view import MeetupList, Meetup, UpcomingMeetup
-from app.api.v2.views.question_view import Question, Upvote
+from app.api.v2.views.question_view import Question, Vote
 from app.api.v2.views.rsvp_view import Rsvp
 from app.api.v2.views.user_view import UserRegistration, UserLogin
 
@@ -23,6 +23,6 @@ API.add_resource(MeetupList, '/meetups')
 API.add_resource(Meetup, '/meetups/<meetup_id>')
 API.add_resource(UpcomingMeetup, '/meetups/upcoming/')
 API.add_resource(Question, '/meetups/<meetup_id>/questions')
-API.add_resource(Upvote, '/questions/<question_id>/upvote')
+API.add_resource(Vote, '/questions/<question_id>/<vote_type>')
 API.add_resource(Rsvp, '/meetups/<meetup_id>/rsvps')
 API.add_resource(Comment, '/questions/<question_id>/comments')
