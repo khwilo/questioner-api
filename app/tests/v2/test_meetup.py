13f03b95a7a40e9ffe9a680f2414ac98a80113fd
khwilo <khwilowatai@gmail.com>
2019-02-18 19:21:40 +0300

    [Bug #164044285] Test the validation for a meetup datetime

--- app/tests/v2/test_meetup.py
+++ app/tests/v2/test_meetup.py
@@ -1,7 +1,7 @@
 """This module represents tests for the meetup entity"""
 import json
 from app.tests.v2.sample_data import ADMIN_LOGIN, MEETUP, USER_REGISTRATION, \
-USER_LOGIN, NEW_MEETUP
+USER_LOGIN, NEW_MEETUP, WRONG_MEETUP_DATETIME
 from app.tests.v2.test_base import BaseTestCase
 
 class MeetupTestCase(BaseTestCase):
@@ -21,6 +21,22 @@ class MeetupTestCase(BaseTestCase):
             data=json.dumps(MEETUP)
         )
         self.assertEqual(res.status_code, 201)
+
+    def test_wrong_meetup_datetime(self):
+        """Validate meetup datetime input value"""
+        res = self.client().post(
+            '/api/v2/auth/login',
+            headers=self.get_accept_content_type_headers(),
+            data=json.dumps(ADMIN_LOGIN)
+        )
+        response_msg = json.loads(res.data.decode("UTF-8"))
+        access_token = response_msg["data"][0]["token"]
+        res = self.client().post(
+            '/api/v2/meetups',
+            headers=self.get_authentication_headers(access_token),
+            data=json.dumps(WRONG_MEETUP_DATETIME)
+        )
+        self.assertEqual(res.status_code, 400)
 
     def test_user_cannot_create_meetup(self):
         """Test a regular user cannot create a meetup"""
